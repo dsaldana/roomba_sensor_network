f01e1e34f8a44c1a509f51d0964c878b76990856
David Saldana <dajulian@gmail.com>
2013-10-17 23:36:34 -0300

    Robot control in launch file

--- roomba_sensor/scripts/roomba_control.py
+++ roomba_sensor/scripts/roomba_control.py
@@ -6,24 +6,28 @@ from geometry_msgs.msg import Twist
 
 def run():
 	# Node roombaControl
-    rospy.init_node('roomba_control')
+	rospy.init_node('roomba_control')
 
+	# Robot's name is an argument
+	robotName = rospy.get_param('~robot_name', 'Robot1')
+	print robotName
 
 	# Control the car.
-    velPub = rospy.Publisher("/Robot1/commands/velocity", Twist)
+	topicName = "/" + robotName + "/commands/velocity"
+	velPub = rospy.Publisher(topicName, Twist)
 
 
-    while not rospy.is_shutdown():
+	while not rospy.is_shutdown():
 		vel = Twist()
-		vel.linear.x = 1.0
+		vel.linear.x = 1.10
 		print("publicando publicando mic mic.")
 		velPub.publish(vel)
 
-		rospy.sleep(1.0)
+		rospy.sleep(0.10)
 
 
 if __name__ == '__main__':
-    try:
-        run()
-    except rospy.ROSInterruptException:
-        pass
+	try:
+		run()
+	except rospy.ROSInterruptException:
+		pass
