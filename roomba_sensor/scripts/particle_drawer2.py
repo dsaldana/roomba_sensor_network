b43e249f6433b0879b33285cb1898b4a2a29bd49
David S <dav@munch-MS-7593.(none)>
2013-11-26 21:57:03 -0200

    drawer is relative to the size

--- roomba_sensor/scripts/particle_drawer2.py
+++ roomba_sensor/scripts/particle_drawer2.py
@@ -29,11 +29,19 @@ window = pygame.display.set_mode((width, height),HWSURFACE|DOUBLEBUF|RESIZABLE)
 def draw_robot(robotX, robotY, robotT, color=(0, 0, 170)):
 	x2 = mx + (robotX - mapX1) * ax / mapLX
 	y2 = my + (-robotY - mapY1) * ay / mapLY
-	rd = 8
+	# Robot radio
+	rd = int(mx / 3)
+	if rd <1:
+		rd = 1
+	# Line for direction
+	linew = int(mx / 15)
+	if linew < 1:
+		linew = 1
+		
 	pygame.draw.circle(window, color, (int(x2), int(y2)), rd, 0)
 	pygame.draw.line(window, (255, 248, 0), 
 		(x2, y2),
-		(x2 + rd * cos(robotT), y2 - rd * sin(robotT)), 2)
+		(x2 + rd * cos(robotT), y2 - rd * sin(robotT)), linew)
 	
 
 def draw_points(points, color=(255,0,0)):
