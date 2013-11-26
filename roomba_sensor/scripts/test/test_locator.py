c50bb61835f98218e4906323962e705a853c8de5
David S <dav@munch-MS-7593.(none)>
2013-11-26 20:51:34 -0200

    show robot in map

--- roomba_sensor/scripts/test/test_locator.py
+++ roomba_sensor/scripts/test/test_locator.py
@@ -5,16 +5,33 @@ from math import *
 
 from roomba_sensor.roomba import RealRoomba
 
+from roomba_comm.msg import Particle
+from roomba_comm.msg import PointR
+
+
 def run():
 	rospy.init_node('test_locator')
 	
 	locator = RealRoomba("Robot1")
-	
+
+	## for visualization
+	# Create a publisher for the particles
+	partPub = rospy.Publisher("/Robot1/particles", Particle)
 	
 	while not rospy.is_shutdown():
 		print locator.get_position()
-		rospy.sleep(0.1)
+		
+		if(locator.get_position() != None):
 
+			# Publish robot position
+			msg_parts = Particle()
+			mrobot = PointR()
+			mrobot.x, mrobot.y, mrobot.z = locator.get_position()
+			msg_parts.mrobot = mrobot
+							
+			partPub.publish(msg_parts)
+
+		rospy.sleep(0.1)
 if __name__ == '__main__':
 	try:
 		run()
