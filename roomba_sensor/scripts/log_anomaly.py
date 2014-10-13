2f87d93c12f8e8f937082b1d828e20d275a7253b
dsaldana <dajulian@gmail.com>
2014-10-13 12:17:30 -0300

    log file working

--- roomba_sensor/scripts/log_anomaly.py
+++ roomba_sensor/scripts/log_anomaly.py
@@ -1,53 +1,31 @@
 #!/usr/bin/env python
-import copy
-import os
 import pickle
-from gazebo_msgs.msg import ModelState
-from gazebo_msgs.srv import SetModelState
-import rospy
-import math
 
-# rostopic pub -r 20 /gazebo/set_model_state gazebo_msgs/ModelState
-# '{model_name: coke_can, pose: { position: { x: 1, y: 0, z: 2 },
-# orientation: {x: 0, y: 0.491983115673, z: 0, w: 0.870604813099 } },
-# twist: { linear: { x: 0, y: 0, z: 0 }, angular: { x: 0, y: 0, z: 0}  }, reference_frame: world }'
-from sets import Set
+import rospy
+import time
 
 from roomba_sensor.comm.communicator import Communicator
 
-LOG_FILE = '/home/dav/testings_anom/log_anomalies.pkl'
+#LOG_FILE = '/home/dav/testings_anom/log_anomalies.pkl'
+LOG_FILE = '/home/dav/testings_anom/log_anomalies{}.pkl'.format(time.clock())
+
 
 def log_anomaly():
     """
     Spawn and move the fire
     """
-
-    existent_anomalies = Set()
-
     communicator = Communicator('Robot1')
-
-    # ## SPAWN
-    # for anom, data in anomalies.items():
-    #     spawn_fire(anom, data[0][0], data[0][1])
-    #     pass
-
-    # ### MOVE
-
     rospy.init_node('log_anomaly')
 
-    rospy.sleep(0.1)
-
-    r = rospy.Rate(2)  # 10hz
-    #
-    # # moving_x = 0
     log = []
 
+    r = rospy.Rate(2)  # 10hz
     while not rospy.is_shutdown():
-        time = rospy.get_rostime().secs + rospy.get_rostime().nsecs / 1000000000.0
-        print time
+        t = rospy.get_rostime().secs + rospy.get_rostime().nsecs / 1000000000.0
+        print t
         orobots, sensed_points, anomaly_polygons = communicator.read_inbox()
 
-        log.append((time, anomaly_polygons, {}, sensed_points))
+        log.append((t, anomaly_polygons, {}, sensed_points))
         with open(LOG_FILE, 'wb') as output:
             pickle.dump(log, output, pickle.HIGHEST_PROTOCOL)
         r.sleep()
