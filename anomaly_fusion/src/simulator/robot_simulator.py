cb1fbcbb4542957832d325ad0ca1ff688c21c057
dsaldana <dajulian@gmail.com>
2015-02-02 21:06:00 -0200

    robot_simulator class

---
+++ anomaly_fusion/src/simulator/robot_simulator.py
@@ -0,0 +1,106 @@
+import math
+from anomaly.AnomalyPredictor import AnomalyPredictor
+import numpy as np
+import matplotlib.pyplot as plt
+from simulator.spiral_generator import generate_spiral
+from matplotlib import animation
+
+class RobotSpiralSimultaor:
+    def __init__(self, duration, start_time=40, x_lim=(-40, 40), y_lim=(-40, 40)):
+        ## parameter
+        self.start_time = start_time
+
+        ######### generate spiral ###################
+        self.path_tracking = generate_spiral(duration)
+
+        ### Create predictor ###
+        self.ap = AnomalyPredictor()
+
+        ##### Init for plotting ######################
+        # create a simple animation
+        self.fig = plt.figure()
+        self.ax = plt.axes(xlim=x_lim, ylim=y_lim)
+        # anomaly circle
+        self.anomaly_circle = plt.Circle((0, 0), 20, color='#ffd5d5', )
+        self.ax.add_patch(self.anomaly_circle)
+
+
+    def animate(self, t):
+        # initialization
+        if t == 0:
+            self.ap._clear_detections()
+            # add old points
+            for i in range(self.start_time):
+                p = self.path_tracking[i]
+                anomaly_time = math.hypot(p[0], p[1])  # time is proportional to the radio.
+                self.ap.add_local_sensed_point(p[3], p, anomaly_time)
+
+        # increase the time
+        t += self.start_time
+
+        ### add new measure
+        p = self.path_tracking[t]  # new measure
+        anomaly_radio = math.hypot(p[0], p[1])
+        anomaly_time = anomaly_radio
+
+        self.ap.add_local_sensed_point(p[3], p, anomaly_time)  # add
+        # adapt polygon to the new information
+        self.ap.modify_polygon(anomaly_time, ddd=20)
+
+        # Estimated polygon
+        estimated_polygon = self.ap.estimator.estimate_polygon(anomaly_time)
+
+
+        ### if the polygon is identified.
+        print t, anomaly_time, self.ap.is_polygon_identified, len(self.ap.polyline), len(self.ap.polyline), len(
+            self.ap.estimator.vertex_path)
+
+        ####################
+        ##### Plot #########
+        ####################
+        self.ax.clear()
+        self.ax.cla()
+        self.ax.plot((-40, 40, 40, -40), (-40, -40, 40, 40))  # bounds
+        # plot full path
+        xx = self.path_tracking[:t + 1][:, 0]
+        yy = self.path_tracking[:t + 1][:, 1]
+        tt = self.path_tracking[t][2] - math.pi / 2
+        path_line = self.ax.plot(xx, yy, 'y.-')
+        # polygon
+        polyline = np.array(self.ap.polyline)
+        px, py = polyline[:, 0], polyline[:, 1]
+        polygon_line = self.ax.plot(px, py, lw=2)
+
+        # Anomaly size
+        self.anomaly_circle.radius = anomaly_radio
+
+        # Draw estimated polygon
+        estimated_pol = None
+        if estimated_polygon:
+            estimated_polygon = np.array(estimated_polygon)
+            estimated_pol = self.ax.plot(estimated_polygon[:, 0], estimated_polygon[:, 1], 'bo-')
+
+        ## paths
+        measurement = (p[0], p[1], anomaly_time)
+        if measurement in self.ap.estimator.vertex_path:
+            # print ap.vertex_path[(p[0], p[1])]
+            last_path = np.array(self.ap.estimator.vertex_path[measurement])
+            track_line = self.ax.plot(last_path[:, 0], last_path[:, 1], 'ro--')
+
+            drawings = [self.anomaly_circle] + path_line + polygon_line + track_line
+
+            if estimated_pol is None:
+                return drawings
+            else:
+                return drawings + estimated_pol
+
+        else:
+            return path_line + polygon_line
+
+    def show(self, interval=10):
+        # Animation
+        ani = animation.FuncAnimation(self.fig, self.animate, frames=len(self.path_tracking) - 1 - self.start_time, interval=interval, blit=True)
+
+        plt.show()
+
+
---
+++ anomaly_fusion/src/simulator/spiral_generator.py
@@ -0,0 +1,28 @@
+import numpy as np
+import math
+
+def generate_spiral(duration):
+    # Espiral en coordenadas polares
+    """
+
+    :param duration:
+    :return: an array of elements [x,y, theta, sensor, time]
+    """
+    time = np.linspace(10, duration, duration * 10)
+
+    # with a=1 and n=4
+    n = 1
+    x = np.cos(time) * time ** (1. / n)
+    y = np.sin(time) * time ** (1. / n)
+    theta = np.arctan2(y, x) + math.pi / 2
+
+    ############### as log data ############3
+    ## Only the path for tracking
+    path_tracking = np.vstack((x, y))  # x,y
+    path_tracking = np.vstack((path_tracking, theta))  # angle
+    path_tracking = np.vstack((path_tracking, np.ones(len(time))))  # sensor
+    path_tracking = np.vstack((path_tracking, range(len(time))))  # time
+    path_tracking = path_tracking.T
+
+
+    return path_tracking
